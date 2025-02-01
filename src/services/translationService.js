072bae486878fb6fbeee99cdf5a01f6cc4d0413f
Abhinav <raiabhinav.in@gmail.com>
2025-02-01 19:18:09 +0530

    translateFAQ function added

--- src/services/translationService.js
+++ src/services/translationService.js
@@ -1,8 +1,9 @@
-const translate = require("google-translate-api");
+const translate = require("google-translate-api-x");
 
 const translateText = async (text, lang) => {
   try {
     const res = await translate(text, { to: lang });
+    console.log(res.text);
     return res.text;
   } catch (err) {
     console.error("Translation Error:", err);
@@ -10,4 +11,16 @@ const translateText = async (text, lang) => {
   }
 };
 
-module.exports = { translateText };
+const translateFAQ = async (question, answer, languages) => {
+  const translations = {};
+  for (const lang of languages) {
+    translations[lang] = {
+      question: await translateText(question, lang),
+      answer: await translateText(answer, lang),
+    };
+  }
+  return translations;
+};
+
+
+module.exports = { translateText, translateFAQ };
