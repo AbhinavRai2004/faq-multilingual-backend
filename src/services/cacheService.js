966aa93d2a362574326f0838da6509689db94e25
Abhinav <raiabhinav.in@gmail.com>
2025-02-01 19:17:23 +0530

    delCache function added

--- src/services/cacheService.js
+++ src/services/cacheService.js
@@ -9,4 +9,9 @@ const setCache = async (key, data, ttl = 3600) => {
     client.set(key, JSON.stringify(data), { EX: ttl });
 };
 
+const delCache = async (key) => {
+    await client.del(key);
+};
+
+
 module.exports = {getCache, setCache};
